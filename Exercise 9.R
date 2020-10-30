55e3f174de9bcc52b6479ccf69cdaa6ad426e3e4
Erica-Tsai <xtsai@nd.edu>
2020-10-30 08:49:30 -0400

    part1

---
+++ Exercise 9.R
@@ -0,0 +1,13 @@
+# Exercise 9
+dir<-function(file)
+  input<-read.csv(file,header = FALSE)
+  
+  total=coefficient,variation(sd,avg)
+  
+  if(total>50){
+    print("This file is correct")
+  }else{
+    print("This file is incorrect")
+  }
+  
+  
